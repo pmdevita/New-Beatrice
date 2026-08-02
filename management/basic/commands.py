286600660967fc06ea333b9557677d4d4168a510
Peter DeVita <mewtwo2643@yahoo.com>
2026-08-02 15:32:06 -0400

    Complete port of basic commands

--- management/basic/commands.py
+++ management/basic/commands.py
@@ -5,14 +5,16 @@ import re
 import hikari
 import atsume
 from atsume.command.context import CommandContext, MessageContext
+from atsume.discord import fetch_guild_channel
 from atsume.settings import settings
-from atsume.command import command, CommandModel
+from atsume.command import command, CommandModel, event
 
 # from audio.data.audio import AudioFile
 # from audio.host import VoiceComponent
 
-from typing import Annotated, Optional
+from typing import Optional
 
+from pydantic import Field
 
 DIABETES = re.compile("my family history has (\\w+)", re.I)
 SOCK_DRAWER = re.compile("there\\'?s nothing happening", re.I)
@@ -23,13 +25,11 @@ HI_FILES = [
 ]
 
 
-# @tanjun.as_message_command("hi", "hello", "hey", "howdy", "beatrice", "beako", "betty")
-
 class HiModel(CommandModel):
     member: Optional[hikari.Member]
 
 
-@command("hi", "Beatrice says hi")
+@command("hi", "Beatrice says hi", aliases=["hello", "hey", "howdy", "beatrice", "beako", "betty"])
 async def hello(ctx: CommandContext | MessageContext, args: HiModel):
     member: hikari.Member | hikari.User = args.member if args.member else ctx.author
     # if isinstance(member, hikari.Member) and ctx.voice:
@@ -60,36 +60,36 @@ async def hello_audio(voice: hikari.api.VoiceComponent, text_channel: hikari.Gui
     await text_channel.send(line[1])
 
 
-# @tanjun.annotations.with_annotated_args(follow_wrapped=True)
-# @tanjun.as_message_command("ping")
-# @tanjun.as_slash_command("ping", "Get the bot's current websocket latency")
-# async def ping(ctx: tanjun.abc.Context):
-#     latency = ctx.component.client.shards.heartbeat_latency
-#     await ctx.respond(f"Hey stop that! ({round(latency * 1000)}ms)")
-#
-#
-# @tanjun.annotations.with_annotated_args(follow_wrapped=True)
-# @tanjun.as_message_command("ban")
-# @tanjun.as_slash_command("ban", "Ban this user!")
-# async def ban(ctx: tanjun.abc.Context, member: Annotated[User, "User to ban"]):
-#     text = f"You're in big trouble {member.mention}, I suppose!"
-#     video_url = "https://cdn.discordapp.com/attachments/984306454133637170/984318182477152306/beatriceban.mov"
-#     await ctx.respond(text)
-#     await ctx.get_channel().send(video_url)
-#
-#
-# @atsume.with_listener
-# async def on_message(message: hikari.events.MessageCreateEvent):
-#     if not message.content:
-#         return
-#     result = DIABETES.findall(message.content)
-#     if result:
-#         await (await message.message.fetch_channel()).send(f"(There is {result[0].lower()} in my family history)")
-#     else:
-#         result = SOCK_DRAWER.findall(message.content)
-#         if result:
-#             await asyncio.sleep(3)
-#             await (await message.message.fetch_channel()).send(
-#                 "I finally got the wildfire in my sock drawer under control!")
+@command(description="Get the bot's current websocket latency")
+async def ping(ctx: CommandContext | MessageContext):
+    latency = ctx.bot.heartbeat_latency
+    await ctx.respond(f"Hey stop that! ({round(latency * 1000)}ms)")
+
+
+class BanModel(CommandModel):
+    user: hikari.Member = Field(description="The user to ban")
+
+
+@command("ban", "Ban this user!")
+async def ban(ctx: CommandContext | MessageContext, args: BanModel):
+    text = f"You're in big trouble {args.user.mention}, I suppose!"
+    video_url = "https://cdn.discordapp.com/attachments/984306454133637170/984318182477152306/beatriceban.mov"
+    await ctx.respond(text)
+    await (await ctx.get_channel()).send(video_url)
+
+
+@event
+async def on_message(bot: hikari.GatewayBot, message: hikari.events.MessageCreateEvent):
+    if not message.content:
+        return
+    result = DIABETES.findall(message.content)
+    if result:
+        await (await fetch_guild_channel(bot, message.channel_id)).send(f"(There is {result[0].lower()} in my family history) l")
+    else:
+        result = SOCK_DRAWER.findall(message.content)
+        if result:
+            await asyncio.sleep(3)
+            await (await fetch_guild_channel(bot, message.channel_id)).send(
+                "I finally got the wildfire in my sock drawer under control! l")
 
 # Todo: Port sound commands (hello, inhale, mouthful)
